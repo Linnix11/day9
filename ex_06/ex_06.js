792ce0befdebe92b0e7f4dcaf6cd9386cab9783e
Linnix11 <arthur.champagne-forterre@epitech.digital>
2024-10-10 12:12:05 +0200

    ex6

---
+++ ex_06/ex_06.js
@@ -0,0 +1,55 @@
+particlesJS('particles-js1', {
+    particles: {
+        number: { value: 80 },
+        size: { value: 4 }
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: 'repulse' }
+        }
+    }
+});
+
+particlesJS('particles-js2', {
+    particles: {
+        number: { value: 100 },
+        color: { value: "#f9c74f" },
+        shape: { type: "star" },
+        size: { value: 6 }
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: 'bubble' }
+        }
+    }
+});
+
+particlesJS('particles-js3', {
+    particles: {
+        number: { value: 150 },
+        shape: { type: "circle" },
+        color: {value: "#05ed24"},
+        size: { value: 3 },
+        move: { speed: 2 }
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: 'grab' }
+        }
+    }
+});
+
+particlesJS('particles-js4', {
+    particles: {
+        number: { value: 50 },
+        shape: { type: "polygon", polygon: { nb_sides: 5 } },
+        size: { value: 5 },
+        color: { value: "#ff6b6b" },
+        move: { speed: 1 }
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: 'push' }
+        }
+    }
+});
